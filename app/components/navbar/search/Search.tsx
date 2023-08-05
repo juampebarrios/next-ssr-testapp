f9f9b4573eca245d886aa7291634ec520ae05fa4
juampebarrios <97.juanbarrios@gmail.com>
2023-08-05 20:13:39 -0300

    files

---
+++ app/components/navbar/search/Search.tsx
@@ -0,0 +1,14 @@
+'use client';
+
+import './Search.scss';
+
+const Search = () => {
+  return (
+    <div className='search'>
+      <span className="item">Anywhere</span>
+      <span className="item">Any Week</span>
+      <span className="item">Add guests</span>
+    </div>
+  );
+}
+export default Search;
