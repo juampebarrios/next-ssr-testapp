de193df5778a45322680734292f96b410e7ad20c
juampebarrios <97.juanbarrios@gmail.com>
2023-08-05 21:49:38 -0300

    coso

--- app/components/navbar/Nabvar.tsx
+++ app/components/navbar/Nabvar.tsx
@@ -2,6 +2,7 @@ import './Navbar.scss';
 import Container from '../generic-components/container/Container';
 import Logo from './logo/Logo';
 import Search from './search/Search';
+import UserMenu from './user-menu/UserMenu';
 
 const Navbar = () => {
   return (
@@ -10,6 +11,7 @@ const Navbar = () => {
         <div className='nav-elements'>
           <Logo/>
           <Search/>
+          <UserMenu/>
         </div>
       </Container>
     </div>
---
+++ app/components/navbar/user-menu/UserMenu.tsx
@@ -0,0 +1,8 @@
+const UserMenu = () => {
+  return (
+    <div className='user-menu'>
+    </div>
+  )
+}
+
+export default UserMenu;
